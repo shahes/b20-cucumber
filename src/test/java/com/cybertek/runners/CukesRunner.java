e1afb5b22103c3bb4663c40dc1259eb510db915b
shahes <esrasahbaz@gmail.com>
2021-01-11 13:34:32 -0500

    pom.xml is updated

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin ={"html:target\\cucumber-report.html","json:target\\cucumber.json",
-        "rerun:target\\rerun.txt"},
+        plugin ={"html:target/cucumber-report.html","json:target/cucumber.json",
+        "rerun:target/rerun.txt"},
         features = "src\\test\\resources\\features",
         glue = "com\\cybertek\\step_definitions",
         dryRun = false,
